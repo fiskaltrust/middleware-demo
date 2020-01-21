cbfabf95782f77c034b3dc821a901f59ff81c3c6
holzpfanne <jonathan.koenig@fiskaltrust.services>
2020-01-21 14:27:02 +0100

    better make process

---
+++ C/REST/Echo/configure.ps1
@@ -0,0 +1,36 @@
+# usage  -WSDL $WSDL -libwsock32 $libwsock32
+
+[CmdletBinding()]
+param (
+    
+    [parameter(HelpMessage="path inside curl lib folder (default: .\lib\curl)")]
+    [string]$PATH_libcurl
+)
+
+Write-Output "Configuring Project"
+
+#libcurl
+if(!$PATH_libcurl) {
+    $PATH_libcurl = Read-Host -Prompt "Please enter path to gsaop lib (default: .\lib\curl)"
+    if(!($PATH_gSOAP)) { $PATH_gSOAP = ".\lib\curl"}
+}
+if(!(Test-Path $PATH_libcurl)) {
+    Write-Output "curl lib not found at: $PATH_libcurl"
+    Exit
+}
+
+#cut '\' if at the end
+if($PATH_libcurl.Substring($PATH_libcurl.get_Length()-1) -eq "\") {
+    $PATH_libcurl = $PATH_libcurl.Substring(0,$PATH_libcurl.get_Length()-1)
+}
+
+#write makefile
+$IN = (Get-Content -Path .\Makefile.in -Raw)
+
+$VARS = "CFLAGS = -I$PATH_libcurl"
+$VARS += "\include"
+
+$OUT = "$VARS`n`n$IN"
+Out-File -FilePath .\Makefile -InputObject $OUT -Encoding utf8
+
+Write-Output "Configuration sucessfull"
