ab934745b3247f4e4536e9204a5fbd69bb4d9d87
holzpfanne <jonathan.koenig@fiskaltrust.services>
2020-01-31 10:53:01 +0100

    CPP REST Sign configure.ps1 update

--- CPP/simple/REST/Sign/configure.ps1
+++ CPP/simple/REST/Sign/configure.ps1
@@ -4,7 +4,10 @@
 param (
     
     [parameter(HelpMessage="path inside cpp-httplib lib folder (default: .\lib\cpp-httplib)")]
-    [string]$PATH_cpp_httplib
+    [string]$PATH_cpp_httplib,
+
+    [parameter(HelpMessage="path inside json lib folder (default: .\lib\json)")]
+    [string]$PATH_json
 )
 
 Write-Output "Configuring Project"
@@ -24,10 +27,26 @@ if($PATH_cpp_httplib.Substring($PATH_cpp_httplib.get_Length()-1) -eq "\") {
     $PATH_cpp_httplib = $PATH_cpp_httplib.Substring(0,$PATH_cpp_httplib.get_Length()-1)
 }
 
+#json
+if(!$PATH_json) {
+    $PATH_json = Read-Host -Prompt "Please enter path to cpp-httplib lib (default: .\lib\json)"
+    if(!($PATH_json)) { $PATH_json = ".\lib\json"}
+}
+if(!(Test-Path -Path $PATH_json)) {
+    Write-Output "json lib not found at $PATH_json"
+    Exit
+}
+
+#cut '\' if at the end
+if($PATH_json.Substring($PATH_json.get_Length()-1) -eq "\") {
+    $PATH_json = $PATH_json.Substring(0,$PATH_json.get_Length()-1)
+}
+
 #write makefile
 $IN = (Get-Content -Path .\Makefile.in -Raw)
 
-$VARS = "PATH_cpp_httplib = $PATH_cpp_httplib"
+$VARS = "PATH_cpp_httplib = `"$PATH_cpp_httplib`"`n"
+$VARS += "PATH_json = `"$PATH_json`""
 
 $OUT = "$VARS`n`n$IN"
 Out-File -FilePath .\Makefile -InputObject $OUT -Encoding utf8
