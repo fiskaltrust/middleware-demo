6833b52ddebe719b746bed80d280dc77cdcd72c4
holzpfanne <jonathan.koenig@fiskaltrust.services>
2020-02-03 12:18:31 +0100

    CPP SOAP jouranl win working

--- CPP/simple/SOAP/journal/src/main.cpp
+++ CPP/simple/SOAP/journal/src/main.cpp
@@ -4,23 +4,28 @@
 #else
     #include <soapH.h>
 #endif
-//#include <inttypes.h> //int64_t, uint64_t
+#include <inttypes.h> //int64_t, uint64_t
 //#include <stdio.h>    /* printf, sprintf */
 //#include <stdlib.h>   /* exit, atoi, malloc, free */
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 #ifdef _WIN32
-#define type_Echo_request _ns1__Echo
-#define type_Echo_response _ns1__EchoResponse
-#define new_echo_request(soap, message) soap_new_set__ns1__Echo(soap, message)
+#define type_journal_request _ns1__Journal
+#define type_journal_response _ns1__JournalResponse
+#define new_journal_request soap_new__ns1__Journal
+#define soap_journal_call soap_call___ns1__Journal
 #else //Linux
-#define type_Echo_request _tempuri__Echo
-#define type_Echo_response _tempuri__EchoResponse
-#define new_echo_request(soap, message) soap_new_set__tempuri__Echo(soap, message)
+#define type_journal_request _tempuri__Echo
+#define type_journal_response _tempuri__EchoResponse
+#define new_echo_request soap_new_set__tempuri__Echo
+#define soap_journal_call
 #endif
+
+vector<int64_t> journal_list = {4707387510509010945 ,5067112530745229313};
 
 std::string &ltrim(std::string &str, const std::string &chars = "\t\n\v\f\r ") {
     str.erase(0, str.find_first_not_of(chars));
@@ -36,20 +41,23 @@ std::string &trim(std::string &str, const std::string &chars = "\t\n\v\f\r ") {
     return ltrim(rtrim(str, chars), chars);
 }
 
-void get_input(string *ServiceURL, string *message) {
+void get_input(string *ServiceURL, string *journal_type) {
 
     // Getting all the input
     // ask for Service URL
     cout << "Please enter the serviceurl of the fiskaltrust.Service: ";
     getline(cin, *ServiceURL);
 
-    // get message
-    cout << "Please enter the message to send in the echo request: ";
-    getline(cin, *message);
+    // journal type
+    cout << "Please choose the journal you want to request\
+            \n1: \"AT DEP7\" \"0x4154 0000 0000 0001\"\
+            \n2: \"FR Ticket\" \"0x4652 0000 0000 0001\"\
+            \n: ";
+    getline(cin, *journal_type);
 
     // trim the input strings
     trim(*ServiceURL);
-    trim(*message);
+    trim(*journal_type);
 
     // if ServiceURL end with '/' -> delete it
     if ((*ServiceURL).back() == '/') {
@@ -57,34 +65,53 @@ void get_input(string *ServiceURL, string *message) {
     }
 }
 
+int64_t *get_journal_type(string journal_type) {
+    int index;
+    if(!sscanf(journal_type.c_str(),"%d",&index)) {
+        cerr << "ERROR input is no number" << endl;
+        exit(-1);
+    }
+
+    if(index > journal_list.size()) {
+        cerr << "ERROR invalid journal type" << endl;
+        exit(-1);
+    }
+    index --;
+    return &journal_list[index];
+}
+
 int main() {
-    cout << "This example sends a echo to the fiskaltrust.Service via SOAP\n";
+    cout << "This example sends a journal request to the fiskaltrust.Service via SOAP\n";
 
-    string ServiceURL, message;
+    int64_t from = 0, to = 0;
 
-    get_input(&ServiceURL, &message);
+    string ServiceURL, journal_type;
+
+    get_input(&ServiceURL, &journal_type);
 
     //init echo class
     struct soap *ft = soap_new();
 
-    class type_Echo_request *Echo_request = new_echo_request(ft, (char*)message.c_str());
+    class type_journal_request *journal_request = soap_new_set__ns1__Journal(ft, get_journal_type(journal_type), &from, &to);
 
-    class type_Echo_response Echo_response;
+    class type_journal_response journal_response;
 
     //make call
     cout << "making call... ";
 
     int soap_response;
     #ifdef _WIN32
-        BasicHttpBinding_USCOREIPOSProxy echo_handler(ServiceURL.c_str());
 
-        soap_response = echo_handler.Echo(Echo_request, Echo_response);
+        soap_response = soap_call___ns1__Journal(ft, ServiceURL.c_str(), nullptr, journal_request, journal_response);
+        //BasicHttpBinding_USCOREIPOSProxy journal_handler(request_URL.c_str());
+
+        //soap_response = journal_handler.Journal(journal_request, journal_response);
     #else
         soap_response = soap_call___tempuri__Echo(ft,ServiceURL.c_str(),NULL, Echo_request, Echo_response));
     #endif
     if(soap_response == SOAP_OK) {
         cout << "OK" << endl;
-        cout << "Response: " << Echo_response.EchoResult << endl;
+        cout << "Response: " << journal_response.JournalResult.__ptr << endl;
     } else {
         cout << "done" << endl;
         soap_print_fault(ft, stderr);
